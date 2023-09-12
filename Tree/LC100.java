3a46178c6bc46a1fcf17d401dd0d8fbb95969803
akjoshi2 <akjoshi2003@gmail.com>
2023-09-11 22:25:49 -0500

    Tree

---
+++ Tree/LC100.java
@@ -0,0 +1,25 @@
+public class LC100 {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+        {
+            return true;
+        }
+        if (p != null && q != null)
+        {
+            return ((p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+        return false;
+    }
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+		}
+	}
+}
