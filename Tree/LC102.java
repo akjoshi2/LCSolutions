41a858ce0b26223c8df912aefe658b669b52f3fb
akjoshi2 <akjoshi2003@gmail.com>
2023-09-20 13:33:32 -0500

    Tree - Level Order Traversal

---
+++ Tree/LC102.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC102 {
+	   public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+        {
+            return ans;
+        }
+        q.add(root);
+        while (!q.isEmpty())
+        {
+            int len = q.size();
+            List<Integer> currList = new ArrayList<>();
+            for (int i = 0; i < len; i++)
+            {
+                TreeNode curr = q.poll();
+                currList.add(curr.val);
+                if (curr.left != null)
+                {
+                    q.add(curr.left);
+                }
+                if (curr.right != null)
+                {
+                    q.add(curr.right);
+                }
+            }
+            ans.add(currList);
+        }
+        return ans;
+    }
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+		}
+	}
+}
